969170f45623126594d8ff16c0645485250036ee
Derek Li <lij@winchesterthurston.org>
2023-10-23 14:10:05 -0700

    cross section without ultrarelativistic assumption

--- src/madread/e-e+_a_tau-tau+/hcs.m
+++ src/madread/e-e+_a_tau-tau+/hcs.m
@@ -1,10 +1,14 @@
-% Hard cross section without Sommerfeld enhancement, in unit of pb.
+% Hard cross section (without Sommerfeld enhancement), in unit of pb.
 function s = hcs(E, M)
-    % Constant.
+    % Coupling constant.
     a = 1/137;
+    % Electron mass.
+    m = 0.511*10^(-3);
     
     Esq = E.^2;
-    s = pi*a^2./(4*Esq).*sqrt(1 - M^2./Esq).*(4/3 + 2/3*M^2./Esq);
+    % s = pi*a^2./(4*Esq).*sqrt(1 - M^2./Esq).*(4/3 + 2/3*M^2./Esq);
+    % Without ultrarelativistic approximation for e+, e-.
+    s = pi*a^2./(12*Esq.^2).*sqrt(Esq-M^2)./sqrt(Esq-m^2).*(2*(2*Esq + m^2 + M^2) + m^2*M^2./Esq);
     % Convert from GeV^-2 to pb.
     s = s / 5.07^2*10^10;
 end
